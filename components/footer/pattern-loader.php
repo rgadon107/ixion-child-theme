ce3e779fce2a37a170443a13669b78edf208f904
Robert Gadon <rgadon107@gmail.com>
2026-04-21 19:38:50 -0500

    Add an anchor ID to the footer call-to-action section.

--- components/footer/pattern-loader.php
+++ components/footer/pattern-loader.php
@@ -12,14 +12,16 @@
  * @param array $args {
  * An associative array of arguments passed from get_template_part().
  *
- * @type string $slug  The slug of the wp_block pattern to retrieve.
- * @type string $class The CSS class name(s) to apply to the section wrapper.
+ * @type string $slug   The slug of the wp_block pattern to retrieve.
+ * @type string $class  The CSS class name(s) to apply to the section wrapper.
+ * @type string $id     The ID attribute to apply to the section wrapper.
  * }
  */
 
 // Extract variables from the $args array passed by get_template_part()
-$slug = $args['slug'] ?? '';
-$wrapper_class = $args['class'] ?? '';
+$slug           = $args['slug'] ?? '';
+$wrapper_class  = $args['class'] ?? '';
+$anchor_id      = $args['id'] ?? '';
 
 if (empty($slug)) {
     return;
@@ -30,7 +32,7 @@ $pattern_post = get_page_by_path( $slug, OBJECT, 'wp_block' );
 
 if ( $pattern_post instanceof WP_Post ) {
 
-    printf( '<section class="%s">', esc_attr( $wrapper_class ) );
+    printf( '<section id="%s" class="%s">', esc_attr( $anchor_id ), esc_attr( $wrapper_class ) );
     echo do_blocks( $pattern_post->post_content );
     echo '</section>';
 
--- footer.php
+++ footer.php
@@ -28,13 +28,15 @@
      * pattern-loader view. This section is intended for marketing conversions.
      *
      * @param array $args {
-     * @type string $slug  The slug of the wp_block post type.
-     * @type string $class CSS classes to be applied to the section wrapper.
+     * @type string $slug       The slug of the wp_block post type.
+     * @type string $class      CSS classes to be applied to the section wrapper.
+     * @type string $anchor_id  The anchor ID to be applied to the section wrapper.
      * }
      */
     get_template_part( 'components/footer/pattern-loader', null, [
-        'slug'  => 'footer-email-signup',
-        'class' => 'footer-cta-wrapper alignfull',
+        'slug'      => 'footer-email-signup',
+        'class'     => 'footer-cta-wrapper alignfull',
+        'id'        => 'join-email-list'
     ] );
 
     /**
