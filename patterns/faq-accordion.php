d46d6386febe24a00a9ca9ab0e076c2a2f928c9c
Robert Gadon <rgadon107@gmail.com>
2026-07-14 21:31:25 -0500

    Register `faq-accordion` design pattern with child-theme.

---
+++ patterns/faq-accordion.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: FAQ Accordion
+ * Slug: ixion-child/faq-accordion
+ * Categories: faq-accordion featured
+ * Description: Style the native WordPress accordion block to display a set of frequently asked questions.
+ * Keywords: featured, accordion, faq accordion, faq-accordion, faq_accordion
+ * Viewport Width: 800
+ */
+?>
