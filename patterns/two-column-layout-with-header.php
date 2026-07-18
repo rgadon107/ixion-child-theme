5d98943f64b2b844e3d319989236a2f28048ebf8
Robert Gadon <rgadon107@gmail.com>
2026-07-18 14:59:24 -0500

    Add PHP metadata docblock to pattern file.

---
+++ patterns/two-column-layout-with-header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Two Column Layout With Header
+ * Slug: ixion-child/two-column-layout-with-header
+ * Categories: two-column-layout-with-header, featured
+ * Description: A custom 2-column block with header to render text in paragraphs or list items.
+ * Keywords: featured, 2 column, 2 column layout, two column, two column layout
+ * Viewport Width: 800
+ */
+?>
