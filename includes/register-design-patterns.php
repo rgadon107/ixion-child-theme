b0915da353d79fa2622d6c9ea5b4cb34fb2c9f58
Robert Gadon <rgadon107@gmail.com>
2026-08-01 23:29:42 -0500

    Register new design pattern 'button-group'.

--- includes/register-design-patterns.php
+++ includes/register-design-patterns.php
@@ -39,6 +39,7 @@ function register_pattern_categories(): void    {
         'two-column-layout-with-header'         => array('label' => __('Two Column Layout With Header', 'ixion-child')),
         'two-column-hero-section'               => array('label' => __('Two Column Hero Section', 'ixion-child')),
         'full-width-three-column-feature'       => array('label' => __('Full-Width 3-Column Circular Feature Trio', 'ixion-child')),
+        'button-group'                          => array('label' => __('Button Group', 'ixion-child')),
     );
 
     foreach ($categories as $slug => $settings) {
