c7b6a6a876f2c0fa10374ac411ef79ef2a5f65a8
Robert Gadon <rgadon107@gmail.com>
2026-08-03 17:06:04 -0500

    Remove secondary button from design.

--- patterns/button-group.php
+++ patterns/button-group.php
@@ -2,10 +2,10 @@
 /**
  * Title: Button Group
  * Slug: ixion-child/button-group
- * Categories: button-group, button-primary, button-secondary, buttons, featured
+ * Categories: button-group, button-primary, buttons, featured
  * Sync Status: unsynced
- * Description: A reusable primary and secondary custom button pattern styled with the color palette and expected behavior.
- * Keywords: featured, button-group, button-primary, button-secondary, buttons
+ * Description: A reusable custom button pattern styled with the color palette and expected behavior.
+ * Keywords: featured, button-group, button-primary, buttons
  * Viewport Width: 800
  */
-?><!-- wp:buttons {"className":"btn-group"} --><div class="wp-block-buttons btn-group"><!-- wp:button {"className":"btn\u002d\u002dprimary","placeholder":"Primary Button Text"} --><div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div><!-- /wp:button --><!-- wp:button {"className":"btn\u002d\u002dsecondary","placeholder":"Secondary Button Text"} --><div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div><!-- /wp:button --></div><!-- /wp:buttons -->
+?><!-- wp:buttons {"className":"btn-group"} --><div class="wp-block-buttons btn-group"><!-- wp:button {"placeholder":"Primary Button Text","metadata":{"name":"Button Label Text"},"className":"btn\u002d\u002dprimary"} --><div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div><!-- /wp:button --></div><!-- /wp:buttons -->
