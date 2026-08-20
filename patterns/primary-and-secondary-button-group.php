b41ff83b2c3b1941f107c2ba0ec6e7a5be1bd0c0
Robert Gadon <rgadon107@gmail.com>
2026-08-20 17:00:31 -0500

    Add new custom design pattern file and register pattern in `$categories` array.

---
+++ patterns/primary-and-secondary-button-group.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Primary and Secondary Button Group
+ * Slug: ixion-child/primary-and-secondary-button-group
+ * Categories: primary-and-secondary-button-group, button-primary, button-secondary, buttons, featured
+ * Sync Status: unsynced
+ * Description: A reusable custom button pattern for two buttons styled with the site color palette and expected behavior.
+ * Keywords: featured, primary-and-secondary-button-group, button-primary, button-secondary, buttons
+ * Viewport Width: 800
+ */
+?><!-- wp:buttons {"className":"btn-group"} --><div class="wp-block-buttons btn-group"><!-- wp:button {"placeholder":"Primary Button Text","metadata":{"name":"Button Label Text"},"className":"btn\u002d\u002dprimary"} --><div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"></a></div><!-- /wp:button --><!-- wp:button {"placeholder":"Secondary Button Text","metadata":{"name":"Button Label Text"},"className":"btn\u002d\u002dsecondary"} --><div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"></a></div><!-- /wp:button --></div><!-- /wp:buttons -->
