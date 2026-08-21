bd70f9f3102ce350827a850917f42aefdeef4dc2
Robert Gadon <rgadon107@gmail.com>
2026-08-20 23:20:24 -0500

    Refresh the pattern file to resolve parsing error.

--- patterns/primary-and-secondary-button-group.php
+++ patterns/primary-and-secondary-button-group.php
@@ -1,11 +1,17 @@
 <?php
 /**
- * Title: Primary and Secondary Button Group V2
- * Slug: ixion-child/primary-and-secondary-button-group-v2
+ * Title: Primary and Secondary Button Group
+ * Slug: ixion-child/primary-and-secondary-button-group
  * Categories: button-secondary, featured
- * Sync Status: unsynced
- * Description: A reusable custom button pattern for two buttons styled with the site color palette and expected behavior.
- * Keywords: featured, primary-and-secondary-button-group, button-primary, button-secondary, buttons
- * Viewport Width: 800
  */
-?><!-- wp:buttons {"className":"btn-group","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} --><div class="wp-block-buttons btn-group is-vertical-layout is-content-justification-center"><!-- wp:button {"placeholder":"Primary Button Text","className":"btn--primary"} --><div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div><!-- /wp:button --><!-- wp:button {"placeholder":"Secondary Button Text","className":"btn--secondary"} --><div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div><!-- /wp:button --></div><!-- /wp:buttons -->
+?>
+<!-- wp:buttons {"className":"btn-group","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-buttons btn-group is-content-justification-center is-vertical-layout">
+    <!-- wp:button {"placeholder":"Primary Button Text","className":"btn--primary"} -->
+    <div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
+    <!-- /wp:button -->
+    <!-- wp:button {"placeholder":"Secondary Button Text","className":"btn--secondary"} -->
+    <div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
+    <!-- /wp:button -->
+</div>
+<!-- /wp:buttons -->
