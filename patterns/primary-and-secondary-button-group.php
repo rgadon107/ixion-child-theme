fd7ba5dc93ce4f8d071e8e694da3a62463211adc
Robert Gadon <rgadon107@gmail.com>
2026-08-21 17:48:29 -0500

    One more time... - Update pattern file with `.editorconfig` preventing the addition of whitespace and newlines that might activate an error by the Gutenberg parser.

--- patterns/primary-and-secondary-button-group.php
+++ patterns/primary-and-secondary-button-group.php
@@ -4,14 +4,4 @@
  * Slug: ixion-child/primary-and-secondary-button-group
  * Categories: button-secondary, featured
  */
-?>
-<!-- wp:buttons {"className":"btn-group","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-buttons btn-group is-content-justification-center is-vertical-layout">
-<!-- wp:button {"placeholder":"Primary Button Text","className":"btn--primary"} -->
-<div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
-<!-- /wp:button -->
-<!-- wp:button {"placeholder":"Secondary Button Text","className":"btn--secondary"} -->
-<div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
-<!-- /wp:button -->
-</div>
-<!-- /wp:buttons -->
+?><!-- wp:buttons {"metadata":{"name":"Primary and Secondary Button Group"},"className":"btn-group","layout":{"type":"flex","justifyContent":"center","orientation":"vertical"}} --><div class="wp-block-buttons btn-group"><!-- wp:button {"placeholder":"Primary Button Text","className":"btn\u002d\u002dprimary"} --><div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"></a></div><!-- /wp:button --><!-- wp:button {"placeholder":"Secondary Button Text","className":"btn\u002d\u002dsecondary"} --><div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"></a></div><!-- /wp:button --></div><!-- /wp:buttons -->
