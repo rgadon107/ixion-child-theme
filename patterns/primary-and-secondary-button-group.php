166b62f45b7a189231010a72b286714f75acfbb3
Robert Gadon <rgadon107@gmail.com>
2026-08-21 17:42:51 -0500

    One more time... - Update pattern file with `.editorconfig` preventing the addition of whitespace and newlines that might activate an error by the Gutenberg parser.

--- patterns/primary-and-secondary-button-group.php
+++ patterns/primary-and-secondary-button-group.php
@@ -7,11 +7,11 @@
 ?>
 <!-- wp:buttons {"className":"btn-group","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-buttons btn-group is-content-justification-center is-vertical-layout">
-    <!-- wp:button {"placeholder":"Primary Button Text","className":"btn--primary"} -->
-    <div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
-    <!-- /wp:button -->
-    <!-- wp:button {"placeholder":"Secondary Button Text","className":"btn--secondary"} -->
-    <div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
-    <!-- /wp:button -->
+<!-- wp:button {"placeholder":"Primary Button Text","className":"btn--primary"} -->
+<div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
+<!-- /wp:button -->
+<!-- wp:button {"placeholder":"Secondary Button Text","className":"btn--secondary"} -->
+<div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"><span class="btn__label"></span></a></div>
+<!-- /wp:button -->
 </div>
 <!-- /wp:buttons -->
