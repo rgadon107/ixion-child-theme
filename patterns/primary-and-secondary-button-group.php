abffa1500f9f27f1f6ba2697d353aa47cac8be43
Robert Gadon <rgadon107@gmail.com>
2026-08-21 22:24:20 -0500

    Augment docblock in pattern file.

--- patterns/primary-and-secondary-button-group.php
+++ patterns/primary-and-secondary-button-group.php
@@ -3,5 +3,9 @@
  * Title: Primary and Secondary Button Group
  * Slug: ixion-child/primary-and-secondary-button-group
  * Categories: button-secondary, featured
+ * Sync Status: unsynced
+ * Description: A reusable custom 2 (two) button pattern for a button group styled with the site color palette.
+ * Keywords: featured, button-secondary, button-group, buttons
+ * Viewport Width: 800
  */
 ?><!-- wp:buttons {"metadata":{"name":"Primary and Secondary Button Group"},"className":"btn-group","layout":{"type":"flex","justifyContent":"center","orientation":"vertical"}} --><div class="wp-block-buttons btn-group"><!-- wp:button {"placeholder":"Primary Button Text","className":"btn\u002d\u002dprimary"} --><div class="wp-block-button btn--primary"><a class="wp-block-button__link wp-element-button" href="#"></a></div><!-- /wp:button --><!-- wp:button {"placeholder":"Secondary Button Text","className":"btn\u002d\u002dsecondary"} --><div class="wp-block-button btn--secondary"><a class="wp-block-button__link wp-element-button" href="#"></a></div><!-- /wp:button --></div><!-- /wp:buttons -->
